7f2c393f84511b7af5e45d9761c62e52d963090c
John Kim Querobines <querobinesj@gmail.com>
2025-01-28 18:44:44 +0800

    fix: add README on mocks folder

--- src/mocks/handlers/tasksRouter/index.ts
+++ src/mocks/handlers/tasksRouter/index.ts
@@ -1,17 +1,1 @@
-import { http, HttpResponse } from 'msw';
-import defaultData from './__fixtures__/default.json';
-import errorData from './__fixtures__/error.json';
-
-export const tasksRouterHandler = [
-  http.get('/api/trpc/taskRouter.getAll', async () => {
-    return HttpResponse.json(defaultData, {
-      status: 200,
-    });
-  }),
-
-  http.get('/api/trpc/taskRouter.getAll', async () => {
-    return HttpResponse.json(errorData, {
-      status: 400,
-    });
-  }),
-];
+export const tasksRouterHandler = [];
