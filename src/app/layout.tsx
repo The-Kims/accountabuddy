fe4214f046b8dfce711e4e94c2472e099b3b9a0e
John Kim Querobines <querobinesj@gmail.com>
2025-01-28 19:30:34 +0800

    chore: msw clean up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import ReactQueryProvider from '@/lib/client/react-query-provider';
 import TRPCProvider from '@/lib/client/trpc-provider';
-import MSWProvider from '@/lib/client/msw-provider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -29,11 +28,9 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <MSWProvider>
-          <ReactQueryProvider>
-            <TRPCProvider>{children}</TRPCProvider>
-          </ReactQueryProvider>
-        </MSWProvider>
+        <ReactQueryProvider>
+          <TRPCProvider>{children}</TRPCProvider>
+        </ReactQueryProvider>
       </body>
     </html>
   );
