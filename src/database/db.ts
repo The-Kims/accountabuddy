f80eca83e766cef678b1bf3c2913ac13b43831f6
John Kim Querobines <jkim.querobines@gmail.com>
2025-02-06 19:19:02 +0800

    feat: fix seed

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,10 +2,12 @@ import { DATABASE_URL } from '@/lib/server/env';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 
+import * as schema from './schema';
+
 const client = new Pool({
   connectionString: DATABASE_URL,
 });
 
-const db = drizzle(client);
+const db = drizzle(client, { schema });
 
 export { db };
